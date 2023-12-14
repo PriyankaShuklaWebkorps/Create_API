8e9e35b4800f8850ec6f3aff5f12ee53e866b2f4
PriyankaShuklaWebkorps <priyanka.shukla@webkorps.com>
2023-12-14 13:29:10 +0530

    first commit

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -12,7 +12,6 @@ class Api::V1::UsersController < ApplicationController
   
     def create
       @user = User.new(user_params)
-  
       if @user.save
         render json: @user, status: 200 
       else
